593d61a5f1e8526e126353024ee186c0517fcf22
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-05-30 17:05:14 +0700

    feat: add thirdweb v5, shadcn

--- minting/src/App.tsx
+++ minting/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ThirdwebProvider, ConnectButton } from "thirdweb/react";
+import { thirdwebClient } from "./lib/thirdweb";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThirdwebProvider>
+      <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+        <div className="py-20">
+
+          <div className="flex justify-center mb-20">
+            <ConnectButton
+              client={thirdwebClient}
+              appMetadata={{
+                name: "Example app",
+                url: "https://example.com",
+              }}
+            />
+          </div>
+        </div>
+      </main>
+    </ThirdwebProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ minting/src/lib/thirdweb.ts
@@ -0,0 +1,5 @@
+import { createThirdwebClient } from "thirdweb";
+
+export const thirdwebClient = createThirdwebClient({
+  clientId: "d367550fec701ae0e3590625da63c486",
+});
